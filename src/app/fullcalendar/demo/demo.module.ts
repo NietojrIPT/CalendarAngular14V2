d379dd6c1f1f8970c59fc1d092e4607b45aef8b2
Jose Nieto <jose.nieto@inglesparatodos.edu.co>
2022-09-18 22:11:14 -0500

    new proyecto full calendargoogle14

---
+++ src/app/fullcalendar/demo/demo.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoComponent } from './demo.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    BrowserAnimationsModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
+  declarations: [DemoComponent],
+  exports: [DemoComponent],
+})
+export class DemoModule {}
